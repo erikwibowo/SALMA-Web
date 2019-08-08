07c240bc03630abcb3c3231690b313c582aecab5
erikwibowo <erikwibo@gmail.com>
2019-08-08 16:17:06 +0700

    update api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -67,6 +67,85 @@ class Api extends CI_Controller {
 		echo json_encode($this->Mapi->kegiatan($id_user, $limit, $offset)->result());
 	}
 
+	public function foto_by_id($id_kegiatan){
+		echo json_encode($this->Mapi->fotoByKegiatan($id_kegiatan)->result());
+	}
+
+	public function kegiatan_save(){
+		$keg = $this->Mapi->getIdKegiatan();
+		if ($keg->num_rows() == 0) {
+			$idkegiatan = "KEG-".date("ymd")."001";
+		}else{
+			foreach ($keg->result() as $key) {
+				$idkegs = substr($key->id_kegiatan, 10, 3)+1;
+			}
+			if (strlen($idkegs) == "1") {
+				$idkegiatan = "KEG-".date("ymd")."00".$idkegs;
+			}elseif (strlen($idkegs) == "2") {
+				$idkegiatan = "KEG-".date("ymd")."0".$idkegs;
+			}else{
+				$idkegiatan = "KEG-".date("ymd").$idkegs;
+			}
+		}
+
+		$nama_kegiatan = $this->input->post('nama_kegiatan');
+		$keterangan_kegiatan = $this->input->post('keterangan_kegiatan');
+		$id_user = $this->input->post('id_user');
+		$jml_foto = $this->input->post('jml_foto');
+
+		$data = array(
+			"id_kegiatan"			=> $idkegiatan,
+			"nama_kegiatan"			=> $nama_kegiatan,
+			"keterangan_kegiatan"	=> $keterangan_kegiatan,
+			"id_user"				=> $id_user
+		);
+		if($this->Mapi->kegiatan_insert($data)){
+			for ($i = 1; $i <= $jml_foto ; $i++) { 
+				$nama_foto = $idkegiatan."-".$i.".jpg";
+				$file_foto = $this->input->post('foto'.$i);
+				$path = "files/kegiatan/".$nama_foto;
+				file_put_contents($path, base64_decode($file_foto));
+
+				$data_foto = array(
+					"id_kegiatan"	=> $idkegiatan,
+					"foto"			=> $nama_foto
+				);
+
+				$this->Mapi->foto_insert($data_foto);
+			}
+			$response = array(
+				"code"	=> 1
+			);
+		}else{
+			$response = array(
+				"code"	=> 0
+			);
+		}
+
+		echo json_encode($response);
+
+	}
+
+	public function hapus_kegiatan(){
+		$path = "files/kegiatan/";
+		$id_kegiatan = $this->input->post('id_kegiatan');
+		if ($this->Mapi->hapus_kegiatan($id_kegiatan)) {
+			foreach ($this->Mapi->fotoByKegiatan($id_kegiatan)->result() as $key) {
+				unlink($path.$key->foto);
+				$this->Mapi->hapus_foto($id_kegiatan);
+			}
+			$response = array(
+				'code'	=> 1
+			);
+		}else{
+			$response = array(
+				'code'	=> 0
+			);
+		}
+
+		echo json_encode($response);
+	}
+
 }
 
 /* End of file Api.php */
--- application/models/Mapi.php
+++ application/models/Mapi.php
@@ -12,13 +12,43 @@ class Mapi extends CI_Model {
 		$this->db->select('a.id_kegiatan, a.id_user, b.nama_user, a.nama_kegiatan, a.keterangan_kegiatan, a.dibuat, (select count(id_kegiatan) from tb_foto where id_kegiatan = a.id_kegiatan) as jml_foto');
 		$this->db->join('tb_user b', 'a.id_user = b.id_user');
 		$this->db->where('a.id_user', $id_user);
+		$this->db->order_by('dibuat', 'desc');
 		return $this->db->get('tb_kegiatan a', $limit, $offset);
+	}
+
+	public function kegiatan_insert($data){
+		return $this->db->insert('tb_kegiatan', $data);
 	}
 
 	public function allkegiatan($id_user){
 		$this->db->where('id_user', $id_user);
+		$this->db->order_by('dibuat', 'desc');
 		return $this->db->get('tb_kegiatan');
 	}
+
+	public function getIdKegiatan(){
+		return $this->db->query("SELECT id_kegiatan FROM tb_kegiatan WHERE DATE(dibuat) = CURDATE() ORDER BY id_kegiatan DESC LIMIT 1");
+	}
+
+	public function foto_insert($data){
+		return $this->db->insert('tb_foto', $data);
+	}
+
+	public function fotoByKegiatan($id_kegiatan){
+		$this->db->where('id_kegiatan', $id_kegiatan);
+		return $this->db->get('tb_foto');
+	}
+
+	public function hapus_kegiatan($id_kegiatan){
+		$this->db->where('id_kegiatan', $id_kegiatan);
+		return $this->db->delete('tb_kegiatan');
+	}
+
+	public function hapus_foto($id_kegiatan){
+		$this->db->where('id_kegiatan', $id_kegiatan);
+		return $this->db->delete('tb_foto');
+	}
+
 }
 
 /* End of file Mapi.php */
